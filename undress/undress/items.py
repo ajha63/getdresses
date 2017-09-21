1369ef30da797d0490858349867f0bf0845be23a
Alvaro Hernandez <alvaro.hernandez@vnc.biz>
2017-09-20 20:04:29 -0400

    Add dresses.py and items.py

--- undress/undress/items.py
+++ undress/undress/items.py
@@ -11,4 +11,10 @@ import scrapy
 class UndressItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    # brand_name = scrapy.Field()
+    # product_name = scrapy.Field()
+    # short_description = scrapy.Field()
+    # price_regular = scrapy.Field()
+    # combi_value1  = scrapy.Field()
+    url_img_product = scrapy.Field()
+    url_product = scrapy.Field()
--- undress/undress/spiders/dresses.py
+++ undress/undress/spiders/dresses.py
@@ -4,11 +4,17 @@ from undress.items import UndressItem
 
 class QuotesSpider(scrapy.Spider):
 	name = "dresses"
-
-	allowed_domains = ['simons.ca']
-	start_urls = ['https://www.simons.ca/en/women-clothing/dresses--6680']
-
+	start_urls = [
+		'https://www.simons.ca/en/women-clothing/dresses--6680',
+	]
 	def parse(self, response):
+		for item in response.css('div.product_card'):
+			yield UndressItem(
+				url_img_product = item.css('img').xpath('@src').extract(),
+				url_product = item.css('a').xpath('@href').extract_first()
+			)
 
-		for item in response.xpath('//*[@class=$val]/div', val='product_card loaded'):
-			
+		strpath = '//*[@id="footer_filter"]/div/div[2]/span[3]/a/@href'
+		next_page = response.xpath(strpath).extract_first()
+		if next_page:
+			yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
