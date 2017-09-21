429a6cb00d7c860bfb1e54a2743b977fee81e733
Alvaro Hernandez <hernandez.alvaro@gmail.com>
2017-09-21 12:46:34 -0400

    start parse_dress

--- undress/undress/spiders/dresses.py
+++ undress/undress/spiders/dresses.py
@@ -9,12 +9,19 @@ class QuotesSpider(scrapy.Spider):
 	]
 	def parse(self, response):
 		for item in response.css('div.product_card'):
-			yield UndressItem(
-				url_img_product = item.css('img').xpath('@src').extract(),
-				url_product = item.css('a').xpath('@href').extract_first()
-			)
+			url_prod = item.css('a').xpath('@href').extract_first()
+			url_img_prod = item.css('a').xpath('@href').extract_first()
+			yield scrapy.Request(path, callback = self.parse_dress)
+			#yield UndressItem(
+			#	url_img_product = item.css('img').xpath('@src').extract(),
+			#	url_product = item.css('a').xpath('@href').extract_first()
+			#)
 
 		strpath = '//*[@id="footer_filter"]/div/div[2]/span[3]/a/@href'
 		next_page = response.xpath(strpath).extract_first()
 		if next_page:
 			yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+
+	def parse_dress(self, response):
+		idItem = 
+
